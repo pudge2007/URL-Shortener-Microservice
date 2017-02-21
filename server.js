8f564b16ab78d6beb73362efbdb019d61511b9c4
Boris <borya.ivchenko@mail.ru>
2017-02-21 16:16:07 +0000

    initial

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+ 
+app.get('/', function(req, res) {
+    res.end('Hello World')
+});
+
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("App now running on port", port);
+});
