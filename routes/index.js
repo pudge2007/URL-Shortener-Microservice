ef231b912161915a0fa87933a19b24c383308150
Boris <borya.ivchenko@mail.ru>
2017-02-22 12:30:32 +0000

    completed

---
+++ routes/index.js
@@ -0,0 +1,55 @@
+module.exports = function(app, db) {
+
+var site_url = "https://ib-shortener-url.herokuapp.com/";
+var urls = db.collection('urls');
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/:url', function(req, res) {
+    urls.findOne({
+        "short_url": site_url + req.params.url
+    }, function(err, result) {
+        if (err) throw err;
+        if (result) {
+            console.log('Redirecting to: ' + result.original_url);
+            res.redirect(result.original_url);
+        } else {
+            res.send({"error": "This url is not on the database."});
+        }
+    });
+})
+
+app.get('/new/:url*', function(req, res) {
+    var url = req.url.slice(5);
+    var result = {}
+    
+    if(validateURL(url)){
+        result = {original_url: url, short_url: site_url + linkGen()}
+	    res.send(result);
+
+        urls.save(result, function(err, res) {
+            if (err) throw err;
+            console.log('Saved ' + result);
+        });
+        
+    } else {
+        result = {"error": "Wrong url format"};
+        res.send(result);
+    }
+});
+
+function linkGen() {
+    var num = Math.floor(100000 + Math.random() * 900000);
+    return num.toString().substring(0, 4);
+}
+
+
+function validateURL(url) {
+    // Regex from https://gist.github.com/dperini/729294
+    var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+    return regex.test(url);
+}
+
+}
--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 var express = require('express');
+var path = require('path');
+var mongo = require('mongodb');
+var routes = require('./routes/index');
 var app = express();
- 
-app.get('/', function(req, res) {
-    res.end('Hello World')
-});
 
+var url = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/url-shortener';
+mongo.MongoClient.connect(url, function (err, db) {
+  
+  if (err) throw new Error('Database failed to connect!');
+  else console.log('Connection established to', url);
+  
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
 
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("App now running on port", port);
+  db.createCollection("urls");
+  
+  routes(app, db);
+  
+  
+  var port = process.env.PORT || 8080;
+  app.listen(port, function() {
+    console.log("App now running on port", port);
+  });
+  
+
 });
