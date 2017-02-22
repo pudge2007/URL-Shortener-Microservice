2b6d585c518f3acbddd93c0bcd3cca4bffe550cb
Boris <borya.ivchenko@mail.ru>
2017-02-22 16:49:15 +0000

    test

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/url-shortener';
+/*var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/url-shortener';
 mongo.MongoClient.connect(url, function (err, db) {
   
   if (err) throw new Error('Database failed to connect!');
   
   console.log('Connection established to', url);
-  routes(app, db);
-  
+  routes(app, db);*/
+  routes(app);
   var port = process.env.PORT || 8080;
   app.listen(port, function() {
     console.log("App now running on port", port);
   });
 
-});
+/*});*/
