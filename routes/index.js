bcea7e220c7bceaeb7c8f3aa919a73c699fff644
Boris <borya.ivchenko@mail.ru>
2017-02-22 14:28:43 +0000

    test

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,28 @@
 module.exports = function(app, db) {
 
-var site_url = "https://ib-shortener-url.herokuapp.com";
+var site_url = "https://ib-shortener-url.herokuapp.com/";
 var urls = db.collection('urls');
 
-app.get('/', function(req, res) {
+app.route('/:url')
+    .get(function(req, res) {
+    res.render('index');
+});
+app.route('/new')
+    .get(function(req, res) {
     res.render('index');
 });
 
-app.get('/:url', function(req, res) {
-    var url = site_url + req.url;
-    if (url != site_url + '/favicon.ico') {
+app.route('/:url')
+    .get(function(req, res) {
+    var url = site_url + req.params.url;
+    if (url != site_url + 'favicon.ico') {
     urls.findOne({
         "short_url": url
     }, function(err, result) {
         if (err) throw err;
         if (result) {
             console.log('Redirecting to: ' + result.original_url);
-            res.redirect(result.original_url);
+            res.redirect(301, result.original_url);
         } else {
             res.send({"error": "This url is not on the database."});
         }
@@ -29,7 +35,7 @@ app.get('/new/:url*', function(req, res) {
     var result = {}
     
     if(validateURL(url)){
-        result = {original_url: url, short_url: site_url + '/' + linkGen()}
+        result = {original_url: url, short_url: site_url + linkGen()}
 	    res.send(result);
 
         urls.save(result, function(err, res) {
